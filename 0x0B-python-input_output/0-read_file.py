a5d59e2d8b72b4ca6c572e81a7647a23250e1b7d
Percy Namba <percy.namba@yahoo.com>
2023-09-12 20:13:06 +0000

    changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@ contains read_file function
 def read_file(filename=""):
     """"reads a file(UTF8 and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+        print(file.read(), end="")
