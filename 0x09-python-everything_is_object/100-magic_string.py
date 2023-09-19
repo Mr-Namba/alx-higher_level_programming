b8e4bf67f61b6f6d905bad8c5ce2063573ccfffd
Percy Namba <percy.namba@yahoo.com>
2023-09-19 12:04:37 +0000

    corrections

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,5 @@
 def magic_string():
     s = ""
-    for i in range(1, n+1):
-        s += "BestSchool" * i + "\n"
+    for i in range(1, n + 1):
+        s += "BestSchool" * i + "$\n"
     return s
