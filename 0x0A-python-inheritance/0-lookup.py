92f8b5ece54eac7ff8cee0f78aa301f049954c95
Percy Namba <percy.namba@yahoo.com>
2023-09-19 18:29:15 +0000

    corrections

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""lookup function"""
-
+"""Returns a function lookup()"""
 
 
 def lookup(obj):
-    """Returns list of attributes"""
-    return (dir(obj))
+    """Function that returns the list of available attributes and methods,
+    f an object
+
+    Args:
+        obj (class): object
+
+    Returns:
+        list: list of available attributes and methods of an object
+    """
+    return dir(obj)
