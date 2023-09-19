e7a92758e50060057d11c384b55331545fc34be5
Percy Namba <percy.namba@yahoo.com>
2023-09-19 18:36:04 +0000

    corrections

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
+"""Defines a class BaseGeometry based on 6-base_geometry.py"""
+
+
 class BaseGeometry:
-    """ Class that defines the attributes of Geometric Shapes """
-
+    """Class BaseGeometry.
+    """
     def area(self):
-        """ Method that defines the area of a geomtric shape """
+        """Area function.
 
+        Raises:
+            Exception: if area is not implemented.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Method that recieves the value property """
+        """Validates value.
 
-        if type(value) is not int:
+        Args:
+            name (str): name of the object.
+            value (int): value of the property.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
