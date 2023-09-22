e77983735afceb481c0fb4d16169a1a89d531101
Percy Namba <percy.namba@yahoo.com>
2023-09-22 13:15:36 +0000

    all at a go

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-""" Contains a function write_file """
+"""Module containing the function write_file"""
 
 
-def number_of_lines(filename=""):
-    """ Function that reads from a file and prints its number of lines nod"""
-    nod = 0
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            nod += 1
-    return nod
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number
+    of characters written.
+
+    Args:
+        filename (str, optional): name of the file. Defaults to "".
+        text (str, optional): string of text to write to file. Defaults to "".
+
+    Returns:
+        int: number of characters written to file.
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        """This method returns the number of characters written to a file."""
+        return f.write(text)
