d19426f28d63ca1da774e80c92759e677265babd
Percy Namba <percy.namba@yahoo.com>
2023-09-30 13:46:37 +0000

    all

--- 0x0C-python-almost_a_circle/101-main.py
+++ 0x0C-python-almost_a_circle/101-main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-LockedClass = __import__('101-locked_class').LockedClass
+""" 101-main """
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
-lc = LockedClass()
-lc.first_name = "John"
-try:
-    lc.last_name = "Snow"
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+if __name__ == "__main__":
+
+    list_rectangles = [Rectangle(100, 40), Rectangle(90, 110, 30, 10), Rectangle(20, 25, 110, 80)]
+    list_squares = [Square(35), Square(15, 70, 50), Square(80, 30, 70)]
+
+    Base.draw(list_rectangles, list_squares)
